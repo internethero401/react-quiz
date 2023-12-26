72bb40a98e7b7a1eb0a32f59faf1bb90666c31e2
internethero401 <ugoku30@gmail.com>
2023-12-26 20:13:20 +0900

    Miss.jsxはOK

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
-import Start from './components/Start';
-import Correct from './components/Correct';
+// import Start from './components/Start';
+// import Correct from './components/Correct';
+import Miss from './components/Miss';
 
 function App() {
   return (
     <div id="while" style={{height: "100%", width: "100%", backgroundColor: "silver"}}>
-      <Start />
-      <p></p>
-      <Correct />
+      <Miss />
     </div>
   );
 }
--- src/components/Miss.jsx
+++ src/components/Miss.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Correct = () => {
+const Miss = () => {
     return (
     <div style={{
         display: "flex",
@@ -20,10 +20,10 @@ const Correct = () => {
         </div>
 
         <div style={{display: "flex", flexDirection: 'column'}}>
-            <div style={{marginTop: "150px", textAlign: "center", fontSize: "65px", fontWeight: "bold", color: "#FFD700"}}>せいかい！</div>
+            <div style={{marginTop: "150px", textAlign: "center", fontSize: "68px", fontWeight: "bold", color: "#FFD700"}}>ざんねん...</div>
             <div style={{
-                margin: "25px 23px",
-                height: "270px",
+                margin: "35px 23px",
+                height: "90px",
                 width: "335px",
                 borderWidth: "6px",
                 borderStyle: "solid",  /* ボーダースタイルを追加 */
@@ -31,27 +31,37 @@ const Correct = () => {
                 borderRadius: "30px",
                 // ここからは中のtextについて 
                 display: "flex",
-                flexDirection: "column",
-                fontSize: "18px"
+                flexDirection: 'column',
+                alignItems: "center",  /* 横方向の中央揃えを追加 */
+                justifyContent: "center"  /* 縦方向の中央揃えを追加 */
             }}>
-                <div style={{margin: "30px 41px"}}>
-                    <div>実はオクラは英語!!</div>
-                    <div>英語圏ではそのまま</div>
-                    <div>「オクラ」と言えば通じます。</div>
-                    <div>ちなみに日本では</div>
-                    <div>陸蓮根 (おかれんこん) という</div>
-                    <div>別名があるのですが、</div>
-                    <div>この呼び方は</div>
-                    <div>あまり浸透していません。</div>
+                <div style={{}}>
+                    <div style={{fontSize: "25px"}}>もう一度挑戦してみよう! !</div>
                 </div>
+                
             </div>
         </div>
 
-        <div style={{display: "flex", margin:"130px 25px", justifyContent: "flex-end"}}>
-            <img src="/direct.png" alt="矢印" style={{marginRight: "10px"}}/>
+        <div style={{
+            display: "flex",
+            flexDirection: 'column',
+            alignItems: "center", // 横方向の中央揃えを追加
+        }}>
             <button style={{
+                height: "70px",
+                width: "240px",
+                color: "white",
+                backgroundColor: "#FFD700",
+                border: "none",
+                borderRadius: "15px",}}>
+                    <div style={{fontSize: "48px"}}>Replay</div>
+            </button>
+            <button style={{
+                marginTop: "30px",
                 height: "40px",
-                width: "180px",
+                width: "160px",
+                fontWeight: "bold",
+                color: "#FFD700",
                 backgroundColor: "white",
                 borderStyle: "solid",
                 borderWidth: "5px",
@@ -64,4 +74,4 @@ const Correct = () => {
   )
 }
 
-export default Correct;
+export default Miss;
